09f998649bb924784b6bdf4d8fe6c07be7d1be00
Lukasz Sack <lukaszsack@lsack.local>
2018-01-16 16:18:30 +0100

    Narrowed down to information that I'm interested in, added saving informations to csv file

--- ryanairapi.py
+++ ryanairapi.py
@@ -1,13 +1,13 @@
 import json
 import requests
-
+import csv
 
 headers = {'Content-Type': 'application/json'}
 
 
 def get_ticket_price():
-    date_from = "2018-01-14"
-    date_to = "2018-01-14"
+    date_from = "2018-01-21"
+    date_to = "2018-01-21"
     api_url = "https://api.ryanair.com/farefinder/3/oneWayFares?&arrivalAirportIataCode=STN" \
               "&departureAirportIataCode=SZZ&language=en&limit=16&market=en-gb&offset=0&outboundDepartureDateFrom=%s" \
               "&outboundDepartureDateTo=%s&priceValueTo=" % (date_from, date_to)
@@ -15,15 +15,19 @@ def get_ticket_price():
 
     if information.status_code == 200:
         return json.loads(information.content)
+
     else:
         return None
-
 
 price_info = get_ticket_price()
 
 if price_info is not None:
-    print("Here's your info: ")
-    for key, value in price_info.items():
-        print('{0}: {1}'.format(key, value).encode('utf-8'))
+    for key, value in price_info['fares'][0]['outbound'].items():
+        print('{0}: {1}'.format(key, value).replace('\u0142', ''), '\n')
+        save = open('test.csv', 'a')
+        save.write('{0}: {1}{2}'.format(key, value, '\n').replace('\u0142', ''))
+        save.close()
+       
 else:
     print("[!] Request Failed")
+
